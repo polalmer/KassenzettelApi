f9d63afd9c8f5265ac5efcc9eff364ca6772fc30
Jonas Palzer <j.palzer@queo.onmicrosoft.com>
2023-08-29 10:15:45 +0200

    implemented Tesseract Engine

--- KassenzettelApi/Controllers/OcrController.cs
+++ KassenzettelApi/Controllers/OcrController.cs
@@ -7,16 +7,18 @@ namespace KassenzettelApi.OcrController;
 [Route("ocr")]
 public class OcrController : ControllerBase
 {
-    private readonly ILogger _logger;
-
-    public OcrController(ILogger logger)
-    {
-        _logger = logger;
-    }
+    private TesseractEngine _engine = new(
+        @"C:\Users\palzer\Desktop\Kassenzettelapp\BE\KassenzettelApi\KassenzettelApi\OcrFiles",
+        "deu",
+        EngineMode.Default);
 
     [HttpGet("GetKassenzettelData")]
     public void AnalysePhoto()
     {
+        string imagePath = @"C:\Users\palzer\Desktop\Kassenzettelapp\BE\KassenzettelApi\KassenzettelApi\OcrFiles\BeispielKassenzettel.png";
+        Pix image = Pix.LoadFromFile(imagePath);
+        Page page = _engine.Process(image);
 
+        Console.WriteLine();
     }
 };
