569f881d2dd632007469dbfd6b9c648bee9d77f5
Jonas Palzer <j.palzer@queo.onmicrosoft.com>
2023-08-29 12:00:42 +0200

    base classes

---
+++ KassenzettelApi/Classes/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KassenzettelApi.Classes;
+
+public class Item
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+
+    public float Price { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+}
---
+++ KassenzettelApi/Classes/Kassenzettel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KassenzettelApi.Classes;
+
+public class Kassenzettel
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+
+    public List<(int count,Item)> Items { get;} = new();
+
+    public string? Shop { get; set; }
+}
