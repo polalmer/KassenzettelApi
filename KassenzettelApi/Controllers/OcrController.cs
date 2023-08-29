6bcee5897f989dbdd08133264b09d4db2fe510cb
Jonas Palzer <j.palzer@queo.onmicrosoft.com>
2023-08-29 09:24:07 +0200

    Setup

---
+++ KassenzettelApi/Controllers/OcrController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tesseract;
+
+namespace KassenzettelApi.OcrController;
+
+[ApiController]
+[Route("ocr")]
+public class OcrController : ControllerBase
+{
+    private readonly ILogger _logger;
+
+    public OcrController(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("GetKassenzettelData")]
+    public void AnalysePhoto()
+    {
+
+    }
+};
